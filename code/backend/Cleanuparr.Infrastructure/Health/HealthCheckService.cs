2b288ec683b8ca64f85ff8b25eaeca6dcca113c1
Flaminel <flm.marius@gmail.com>
2026-08-25 16:58:52 +0300

    fixed app crashing on unknown enum values

--- code/backend/Cleanuparr.Infrastructure/Health/HealthCheckService.cs
+++ code/backend/Cleanuparr.Infrastructure/Health/HealthCheckService.cs
@@ -1,4 +1,5 @@
-﻿using Cleanuparr.Infrastructure.Features.Arr.Interfaces;
+﻿using Cleanuparr.Domain.Enums;
+using Cleanuparr.Infrastructure.Features.Arr.Interfaces;
 using Cleanuparr.Infrastructure.Features.DownloadClient;
 using Cleanuparr.Persistence;
 using Microsoft.EntityFrameworkCore;
@@ -112,9 +113,11 @@ public class HealthCheckService : IHealthCheckService
             await using var dataContext = scope.ServiceProvider.GetRequiredService<DataContext>();
             
             // Get all enabled client configurations
-            var enabledClients = await dataContext.DownloadClients
-                .Where(x => x.Enabled)
-                .ToListAsync();
+            var enabledClients = (await dataContext.DownloadClients
+                    .Where(x => x.Enabled)
+                    .ToListAsync())
+                .Where(x => !EnumSentinel.IsUnknown(x.TypeName))
+                .ToList();
             var results = new Dictionary<Guid, HealthStatus>();
             
             // Check health of each enabled client
@@ -233,9 +236,11 @@ public class HealthCheckService : IHealthCheckService
 
             // Get all enabled arr instances across all configs
             // Load configs with instances first, then flatten in memory (SQLite doesn't support APPLY)
-            var configs = await dataContext.ArrConfigs
-                .Include(x => x.Instances)
-                .ToListAsync();
+            var configs = (await dataContext.ArrConfigs
+                    .Include(x => x.Instances)
+                    .ToListAsync())
+                .Where(x => !EnumSentinel.IsUnknown(x.Type))
+                .ToList();
 
             var enabledInstances = configs
                 .SelectMany(c => c.Instances
